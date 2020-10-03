b19e6270f5ae191cae154d80dcba809fc96c07bd
wataflai <67923669+AnthonyAlvarez08@users.noreply.github.com>
2020-10-03 12:27:45 -0400

    almost infinite loop

---
+++ teehee.py
@@ -0,0 +1,6 @@
+# this is just a while true loop, which means I can't truly calculate how many files it will get but meh
+import sys
+wah = float("-inf")
+while True:
+  b = open(f"file{wah}", "w")
+  wah += (1 / float("inf"))
